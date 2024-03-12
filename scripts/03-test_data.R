48974b126b21f7ddd0f55c258b4c24a738be0fa2
Geunchul Shin <joey.shin@mail.utoronto.ca>
2024-03-12 13:38:12 -0400

    updated data tests

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -9,7 +9,18 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+
 #### Test data ####
-head(analysis_data)
+# test that issued_year is numeric
+all(sapply(analysis_data$Issued_year, is.numeric))
+
+# test for range and validity
+all(analysis_data$Num_Endorsements >= 0)
+all(analysis_data$Issued_year >= 2000 & analysis_data$Issued_year <= format(Sys.Date(), "%Y"))
+all(analysis_data$Issue_duration > 0)
+
+# test for missing value
+!any(is.na(analysis_data$Category))
+!any(is.na(analysis_data$Num_Endorsements))
+!any(is.na(analysis_data$Issue_duration))
